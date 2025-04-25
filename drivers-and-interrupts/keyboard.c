a3af83925c1d8d75967d9df827e13c7835cd805a
jbettini <ashhxtm@gmail.com>
2025-04-25 19:54:18 +0200

    add ignored files, drivers handle scancode but didnt recognize

--- drivers-and-interrupts/keyboard.c
+++ drivers-and-interrupts/keyboard.c
@@ -1,25 +1,31 @@
 #include "keyboard.h"
 
-void keyboard_bind_key(keyboard_key_t key)
+void keyboard_init_released(void)
 {
-	if (key.keycode > 256)
-		kpanic("ERROR: bad parameters to keyboard_register_routine\n");
-	current_layout[key.keycode] = (scancode_routine_t){.handler = handler, .key = key};
-}
+	for (int i = 0; i < 256; ++i) {
+		if (press_keys[i].keycode != 0) {
+			uint8_t keycode = (i | 0x80);
+			keyboard_key_t *key = &release_keys[keycode];
+			key->value = press_keys[i].value;
+			key->name = press_keys[i].name;
+			key->keycode = keycode;
+			key->status = RELEASE;
 
-void keyboard_unbind_key(uint8_t keycode) { current_layout[keycode] = UNDEFINED_ROUTINE; }
-
-void keyboard_init(void)
-{
-	for (int i = 0; i < 256; ++i)
-		current_layout[i] = UNDEFINED_ROUTINE;
-	keyboard_init_default_table();
-	keyboard_remap_layout(default_key_table, KEY_MAX);
-}
-
-void keyboard_handle(void)
-{
-	if (inb(0x64) & 0x01) {          // read status
-		uint8_t keycode = inb(0x60); // read data
+		}
 	}
+}
+
+irqreturn_t keyboard_handler(int irq, void *dev_id)
+{
+	if (inb(0x64) & 0x01) {          										// read status
+		uint8_t keycode = inb(0x60); 										// read data
+		if (release_keys[keycode].keycode != 0) {
+			printk(KERN_ALERT "Key number %d pressed\n", keycode);
+		} else if (release_keys[keycode].keycode != 0) {
+			printk(KERN_ALERT "Key number %d pressed\n", keycode);
+		} else {
+			printk(KERN_ALERT "Current layout did not handle the keycode number %d\n", keycode);		
+		}
+	}
+	return IRQ_HANDLED;
 }
--- drivers-and-interrupts/keyboard.h
+++ drivers-and-interrupts/keyboard.h
@@ -1,5 +1,17 @@
 #pragma once
 
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/miscdevice.h>
+#include <asm/irq.h>
+#include <linux/signal.h>
+#include <linux/sched.h>
+#include <linux/interrupt.h>
+#include <asm/io.h>
+#include <linux/module.h>
+
 #define PRESS 1
 #define RELEASE 0
 
@@ -11,106 +23,108 @@ typedef struct keyboard_key {
     bool            status;
 }   keyboard_key_t;
 
-static keyboard_key_t default_key_table[256];
+void keyboard_init_released(void);
+irqreturn_t keyboard_handler(int irq, void *dev_id);
 
-static keyboard_key_t press_keys[] = {
+static keyboard_key_t release_keys[256];
+static keyboard_key_t press_keys[256] = {
 
     // letters
-    {'a', "A", 0x1E, PRESS},
-    {'b', "B", 0x30, PRESS},
-    {'c', "C", 0x2E, PRESS},
-    {'d', "D", 0x20, PRESS},
-    {'e', "E", 0x12, PRESS},
-    {'f', "F", 0x21, PRESS},
-    {'g', "G", 0x22, PRESS},
-    {'h', "H", 0x23, PRESS},
-    {'i', "I", 0x17, PRESS},
-    {'j', "J", 0x24, PRESS},
-    {'k', "K", 0x25, PRESS},
-    {'l', "L", 0x26, PRESS},
-    {'m', "M", 0x32, PRESS},
-    {'n', "N", 0x31, PRESS},
-    {'o', "O", 0x18, PRESS},
-    {'p', "P", 0x19, PRESS},
-    {'q', "Q", 0x10, PRESS},
-    {'r', "R", 0x13, PRESS},
-    {'s', "S", 0x1F, PRESS},
-    {'t', "T", 0x14, PRESS},
-    {'u', "U", 0x16, PRESS},
-    {'v', "V", 0x2F, PRESS},
-    {'w', "W", 0x11, PRESS},
-    {'x', "X", 0x2D, PRESS},
-    {'y', "Y", 0x15, PRESS},
-    {'z', "Z", 0x2C, PRESS},
+    [0x1E] =    {'a',   "A",                    0x1E, PRESS},
+    [0x30] =    {'b',   "B",                    0x30, PRESS},
+    [0x2E] =    {'c',   "C",                    0x2E, PRESS},
+    [0x20] =    {'d',   "D",                    0x20, PRESS},
+    [0x12] =    {'e',   "E",                    0x12, PRESS},
+    [0x21] =    {'f',   "F",                    0x21, PRESS},
+    [0x22] =    {'g',   "G",                    0x22, PRESS},
+    [0x23] =    {'h',   "H",                    0x23, PRESS},
+    [0x17] =    {'i',   "I",                    0x17, PRESS},
+    [0x24] =    {'j',   "J",                    0x24, PRESS},
+    [0x25] =    {'k',   "K",                    0x25, PRESS},
+    [0x26] =    {'l',   "L",                    0x26, PRESS},
+    [0x32] =    {'m',   "M",                    0x32, PRESS},
+    [0x31] =    {'n',   "N",                    0x31, PRESS},
+    [0x18] =    {'o',   "O",                    0x18, PRESS},
+    [0x19] =    {'p',   "P",                    0x19, PRESS},
+    [0x10] =    {'q',   "Q",                    0x10, PRESS},
+    [0x13] =    {'r',   "R",                    0x13, PRESS},
+    [0x1F] =    {'s',   "S",                    0x1F, PRESS},
+    [0x14] =    {'t',   "T",                    0x14, PRESS},
+    [0x16] =    {'u',   "U",                    0x16, PRESS},
+    [0x2F] =    {'v',   "V",                    0x2F, PRESS},
+    [0x11] =    {'w',   "W",                    0x11, PRESS},
+    [0x2D] =    {'x',   "X",                    0x2D, PRESS},
+    [0x15] =    {'y',   "Y",                    0x15, PRESS},
+    [0x2C] =    {'z',   "Z",                    0x2C, PRESS},
     // top keysPRESS
-    {'1', "1", 0x02, PRESS},
-    {'2', "2", 0x03, PRESS},
-    {'3', "3", 0x04, PRESS},
-    {'4', "4", 0x05, PRESS},
-    {'5', "5", 0x06, PRESS},
-    {'6', "6", 0x07, PRESS},
-    {'7', "7", 0x08, PRESS},
-    {'8', "8", 0x09, PRESS},
-    {'9', "9", 0x0A, PRESS},
-    {'0', "0", 0x0B, PRESS},
+    [0x02] =    {'1',   "1",                    0x02, PRESS},
+    [0x03] =    {'2',   "2",                    0x03, PRESS},
+    [0x04] =    {'3',   "3",                    0x04, PRESS},
+    [0x05] =    {'4',   "4",                    0x05, PRESS},
+    [0x06] =    {'5',   "5",                    0x06, PRESS},
+    [0x07] =    {'6',   "6",                    0x07, PRESS},
+    [0x08] =    {'7',   "7",                    0x08, PRESS},
+    [0x09] =    {'8',   "8",                    0x09, PRESS},
+    [0x0A] =    {'9',   "9",                    0x0A, PRESS},
+    [0x0B] =    {'0',   "0",                    0x0B, PRESS},
     // specials
-    {'-',  "MINUS",      0x0C, PRESS},
-    {'=',  "EQUALS",     0x0D, PRESS},
-    {'[',  "LBRACKET",   0x1A, PRESS},
-    {']',  "RBRACKET",   0x1B, PRESS},
-    {';',  "SEMICOLON",  0x27, PRESS},
-    {'\'', "APOSTROPHE", 0x28, PRESS},
-    {'`',  "BACKTICK",   0x29, PRESS}, 
-    {'\\', "BACKSLASH",  0x2B, PRESS},
-    {',',  "COMMA",      0x33, PRESS},
-    {'.',  "PERIOD",     0x34, PRESS},
-    {'/',  "SLASH",      0x35, PRESS},
+    [0x0C] =    {'-',   "MINUS",                0x0C, PRESS},
+    [0x0D] =    {'=',   "EQUALS",               0x0D, PRESS},
+    [0x1A] =    {'[',   "LBRACKET",             0x1A, PRESS},
+    [0x1B] =    {']',   "RBRACKET",             0x1B, PRESS},
+    [0x27] =    {';',   "SEMICOLON",            0x27, PRESS},
+    [0x28] =    {'\'',  "APOSTROPHE",           0x28, PRESS},
+    [0x29] =    {'`',   "BACKTICK",             0x29, PRESS}, 
+    [0x2B] =    {'\\',  "BACKSLASH",            0x2B, PRESS},
+    [0x33] =    {',',   "COMMA",                0x33, PRESS},
+    [0x34] =    {'.',   "PERIOD",               0x34, PRESS},
+    [0x35] =    {'/',   "SLASH",                0x35, PRESS},
     // Space and Tab
-    {' ',  "SPACE", 0x39, PRESS},
-    {'\t', "TAB",   0x0F, PRESS},
+    [0x39] =    {' ',   "SPACE",                0x39, PRESS},
+    [0x0F] =    {'\t',  "TAB",                  0x0F, PRESS},
     // Num pad (main operators)
-    {'*', "NUMPAD_ASTERISK", 0x37, PRESS},
-    {'-', "NUMPAD_MINUS",    0x4A, PRESS},
-    {'+', "NUMPAD_PLUS",     0x4E, PRESS},
+    [0x37] =    {'*',   "NUMPAD_ASTERISK",      0x37, PRESS},
+    [0x4A] =    {'-',   "NUMPAD_MINUS",         0x4A, PRESS},
+    [0x4E] =    {'+',   "NUMPAD_PLUS",          0x4E, PRESS},
     // Control 
-    {0, "LEFT_CTRL",   0x1D, PRESS},
-    {0, "LEFT_SHIFT",  0x2A, PRESS},
-    {0, "RIGHT_SHIFT", 0x36, PRESS},
-    {0, "LEFT_ALT",    0x38, PRESS},
+    [0x1D] =    {0,     "LEFT_CTRL",            0x1D, PRESS},
+    [0x2A] =    {0,     "LEFT_SHIFT",           0x2A, PRESS},
+    [0x36] =    {0,     "RIGHT_SHIFT",          0x36, PRESS},
+    [0x38] =    {0,     "LEFT_ALT",             0x38, PRESS},
     // Toggle (Lock keys)
-    {0, "CAPS_LOCK",   0x3A, PRESS},
-    {0, "NUM_LOCK",    0x45, PRESS},
-    {0, "SCROLL_LOCK", 0x46, PRESS},
+    [0x3A] =    {0,     "CAPS_LOCK",            0x3A, PRESS},
+    [0x45] =    {0,     "NUM_LOCK",             0x45, PRESS},
+    [0x46] =    {0,     "SCROLL_LOCK",          0x46, PRESS},
     // Nav Keys
-    {'7', "NUMPAD_7/HOME",    0x47, PRESS},
-    {'8', "NUMPAD_8/ARROW_UP",    0x48, PRESS},
-    {'9', "NUMPAD_9/PAGE_UP",     0x49, PRESS},
-    {'4', "NUMPAD_4/ARROW_LEFT",  0x4B, PRESS},
-    {'5', "NUMPAD_5/CENTER",      0x4C, PRESS},
-    {'6', "NUMPAD_6/ARROW_RIGHT", 0x4D, PRESS},
-    {'1', "NUMPAD_1/END",         0x4F, PRESS},
-    {'2', "NUMPAD_2/ARROW_DOWN",  0x50, PRESS},
-    {'3', "NUMPAD_3/PAGE_DOWN",   0x51, PRESS},
-    {'0', "NUMPAD_0/INSERT",      0x52, PRESS},
-    {'.', "NUMPAD_PERIOD/DELETE", 0x53, PRESS},
+    [0x47] =    {'7',   "NUMPAD_7/HOME",        0x47, PRESS},
+    [0x48] =    {'8',   "NUMPAD_8/ARROW_UP",    0x48, PRESS},
+    [0x49] =    {'9',   "NUMPAD_9/PAGE_UP",     0x49, PRESS},
+    [0x4B] =    {'4',   "NUMPAD_4/ARROW_LEFT",  0x4B, PRESS},
+    [0x4C] =    {'5',   "NUMPAD_5/CENTER",      0x4C, PRESS},
+    [0x4D] =    {'6',   "NUMPAD_6/ARROW_RIGHT", 0x4D, PRESS},
+    [0x4F] =    {'1',   "NUMPAD_1/END",         0x4F, PRESS},
+    [0x50] =    {'2',   "NUMPAD_2/ARROW_DOWN",  0x50, PRESS},
+    [0x51] =    {'3',   "NUMPAD_3/PAGE_DOWN",   0x51, PRESS},
+    [0x52] =    {'0',   "NUMPAD_0/INSERT",      0x52, PRESS},
+    [0x53] =    {'.',   "NUMPAD_PERIOD/DELETE", 0x53, PRESS},
     // Functions
-    {0, "F1",  0x3B, PRESS},
-    {0, "F2",  0x3C, PRESS},
-    {0, "F3",  0x3D, PRESS},
-    {0, "F4",  0x3E, PRESS},
-    {0, "F5",  0x3F, PRESS},
-    {0, "F6",  0x40, PRESS},
-    {0, "F7",  0x41, PRESS},
-    {0, "F8",  0x42, PRESS},
-    {0, "F9",  0x43, PRESS},
-    {0, "F10", 0x44, PRESS},
-    {0, "F11", 0x57, PRESS},
-    {0, "F12", 0x58, PRESS},
+    [0x3B] =    {0,     "F1",                   0x3B, PRESS},
+    [0x3C] =    {0,     "F2",                   0x3C, PRESS},
+    [0x3D] =    {0,     "F3",                   0x3D, PRESS},
+    [0x3E] =    {0,     "F4",                   0x3E, PRESS},
+    [0x3F] =    {0,     "F5",                   0x3F, PRESS},
+    [0x40] =    {0,     "F6",                   0x40, PRESS},
+    [0x41] =    {0,     "F7",                   0x41, PRESS},
+    [0x42] =    {0,     "F8",                   0x42, PRESS},
+    [0x43] =    {0,     "F9",                   0x43, PRESS},
+    [0x44] =    {0,     "F10",                  0x44, PRESS},
+    [0x57] =    {0,     "F11",                  0x57, PRESS},
+    [0x58] =    {0,     "F12",                  0x58, PRESS},
     // Specials
-    {0, "BACKSPACE", 0x0E, PRESS},
-    {0, "ENTER",     0x1C, PRESS},
-    {0, "ESCAPE",    0x01, PRESS},
-    {0, "LEFT_SUPER", 0x5B, PRESS},            // Touche Windows gauche
-    {0, "RIGHT_SUPER", 0x5C, PRESS},           // Touche Windows droite
-    {0, "MENU", 0x5D, PRESS},                  // Touche menu/application
+    [0x0E] =    {0,     "BACKSPACE",            0x0E, PRESS},
+    [0x1C] =    {0,     "ENTER",                0x1C, PRESS},
+    [0x01] =    {0,     "ESCAPE",               0x01, PRESS},
+    [0x5B] =    {0,     "LEFT_SUPER",           0x5B, PRESS},
+    [0x5C] =    {0,     "RIGHT_SUPER",          0x5C, PRESS},
+    [0x5D] =    {0,     "MENU",                 0x5D, PRESS},
 };
